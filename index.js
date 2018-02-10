d399f1eb853305836f5c6e50c2ab4637fe4d879b
Unknown <schorschdanzer@web.de>
2018-02-10 16:44:41 +0100

    rpc request for blockcount

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var oled = require('./oled');
 var font = require('oled-font-5x7');
 var moment = require('moment');
 
+var request = require('request');
+
 var opts = {
   width: 128,
   height: 64,
@@ -18,6 +20,24 @@ oled.clearDisplay();
 oled.update();
 
 console.log('display clear');
+
+var options = {
+  url: 'http://127.0.0.1:18081/json',
+  headers: {
+    'content-type': 'application/json'
+  },
+  body: {
+    "jsonrpc":"2.0",
+    "id":"0",
+    "method":"getblockcount"
+  }
+};
+
+request(options, function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
 
 setInterval(function () {
   oled.clearDisplay();
