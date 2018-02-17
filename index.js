8226b437f36ec2d0fc511fc935cd3199ba972bfa
Unknown <schorschdanzer@web.de>
2018-02-17 18:24:59 +0100

    blockheight/knownHeight

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var nodeCleanup = require('node-cleanup');
 var request = require('request');
 
 var status = 'loading...';
-var blockHeight = 'loading...'
+var netWorkheight = 'loading...';
+var blockHeight = 'loading...';
 
 var opts = {
   width: 128,
@@ -30,7 +31,7 @@ var options = {
   headers: {
     'content-type': 'application/json'
   },
-  body: JSON.stringify({"jsonrpc":"2.0","id":"0","method":"getblockcount"})
+  body: JSON.stringify({"jsonrpc":"2.0","id":"0","method":"get_info"})
 };
 
 setInterval(function () {
@@ -43,7 +44,8 @@ setInterval(function () {
       try {
         const data = JSON.parse(body)
         status = JSON.stringify(data.result.status);
-        blockHeight = JSON.stringify(data.result.count);
+        netWorkheight = JSON.stringify(data.result.height);
+        blockHeight = JSON.stringify(data.result.target_height - 1);
         console.log(moment().format('MMMM Do YYYY, h:mm:ss a') + ': Requested blockheight from ' + process.argv[2] + ', -> ' + data.result.count);
       } catch (e) {
         console.log(e);
@@ -62,6 +64,8 @@ setInterval(function () {
   oled.writeString(font, 1, 'status: ' + status || 'error', 1, true);
   oled.setCursor(1, 53);
   oled.writeString(font, 1, blockHeight || 'error', 1, true);
+  oled.writeString(font, 1, '/', 1, true);
+  oled.writeString(font, 1, netWorkheight || 'error', 1, true);
 }, 1000);
 
 nodeCleanup(function (exitCode, signal) {
